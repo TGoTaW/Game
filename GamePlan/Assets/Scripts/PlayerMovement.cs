cf1784dfa5f3f564a6fc6d5cf11f80a70e0cad76
Michael T. Sheron <michael.sheron@yahoo.com>
2016-03-07 12:55:27 -0500

    did some things
    player controller script, made a player and attached script to it.

---
+++ GamePlan/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 1.0f;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate (Vector3.right * speed);
+		}
+
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate (Vector3.forward * speed);
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate (Vector3.left * speed);
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate (Vector3.back * speed);
+		}
+
+
+	}
+}
