1f1037207c8dc90ef53b95170553c1b2481fee65
Michael T. Sheron <michael.sheron@yahoo.com>
2016-03-07 13:36:57 -0500

    More player controller things
    huuu hah whogahoo

--- GamePlan/Assets/Scripts/PlayerMovement.cs
+++ GamePlan/Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey (KeyCode.D)) {
+		/*if (Input.GetKey (KeyCode.D)) {
 			transform.Translate (Vector3.right * speed);
 		}
 
@@ -21,8 +21,18 @@ public class PlayerMovement : MonoBehaviour {
 
 		if (Input.GetKey (KeyCode.S)) {
 			transform.Translate (Vector3.back * speed);
+		}*/
+
+		if (Input.GetAxis("Horizontal") > 0 ) {
+			transform.Translate (Vector3.right * speed);
+		} else if (Input.GetAxis("Horizontal") < 0 ) {
+			transform.Translate (Vector3.left * speed);
 		}
 
-
+		if (Input.GetAxis("Vertical") > 0 ) {
+			transform.Translate (Vector3.forward * speed);
+		} else if (Input.GetAxis("Vertical") < 0 ) {
+			transform.Translate (Vector3.back * speed);
+		}
 	}
 }
