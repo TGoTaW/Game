e7d074c0db5dee992342da017675b0d9259c613b
Michael T. Sheron <michael.sheron@yahoo.com>
2016-03-09 11:50:33 -0500

    Player, Camera Controllers.
    hue this is hard

--- GamePlan/Assets/Scripts/PlayerMovement.cs
+++ GamePlan/Assets/Scripts/PlayerMovement.cs
@@ -5,35 +5,13 @@ public class PlayerMovement : MonoBehaviour {
 
 	public float speed = 1.0f;
 	private Vector3 moveDirection = Vector3.zero;
-	private Vector3 lookDirection = Vector3.zero;
-	Animator animator;
-	private int rotationSpeed = 2;
 
 	void Start(){
-		animator = GetComponent<Animator> ();
 
 	}
 
 	// Update is called once per frame
 	void Update () {
-
-
-		/*if (Input.GetKey (KeyCode.D)) {
-			transform.Translate (Vector3.right * speed);
-		}
-
-		if (Input.GetKey (KeyCode.W)) {
-			transform.Translate (Vector3.forward * speed);
-		}
-
-		if (Input.GetKey (KeyCode.A)) {
-			transform.Translate (Vector3.left * speed);
-		}
-
-		if (Input.GetKey (KeyCode.S)) {
-			transform.Translate (Vector3.back * speed);
-		}*/
-
 		moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
 		if (Input.GetAxisRaw ("Horizontal") != 0 || Input.GetAxisRaw ("Vertical") != 0) {
 			transform.rotation = Quaternion.LookRotation (moveDirection);
@@ -42,18 +20,12 @@ public class PlayerMovement : MonoBehaviour {
 		moveDirection *= speed * Time.deltaTime;
 		transform.Translate (moveDirection, Space.World);
 
-
-		
-
-
-		if (Input.GetKey (KeyCode.B)) {
-			
-
+		if(Input.GetButtonDown("Jump")) {
+			Jump();
 		}
-			
+	}
 
-
-
+	private void Jump(){
 
 	}
 }
