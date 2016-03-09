58bfbbe1c6ec14aa8cd32ed60a92f630287ff167
Michael T. Sheron <michael.sheron@yahoo.com>
2016-03-09 14:23:35 -0500

    Camera rotates with q + e, made some scene changes
    I shrunk the dudes and added more enemies and moved the camera a bit.
    player movement does not change with camera, so you go the wrong way
    unless you are facing a certain direction. Working on that now.

--- GamePlan/Assets/Scripts/Camera/CameraController.cs
+++ GamePlan/Assets/Scripts/Camera/CameraController.cs
@@ -4,24 +4,30 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
 	public float dampTime = 0.15f;
-	private Vector3 velocity = Vector3.zero;
 	private Transform target;
-	private Camera camera;
+
+	private Vector3 velocity = Vector3.zero;
+	private Vector3 offset;
+	private Camera c;
 
 	// Use this for initialization
 	void Start () {
-		target = GameObject.Find ("Player").transform;
-		camera = GetComponent<Camera>();
+		if(target == null) {
+			target = GameObject.Find ("Player").transform;
+		}
+		if (c == null) {
+			c = GetComponent<Camera> ();
+		}
+		offset = new Vector3 (0.0f, 120.0f, -120.0f);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (target)
-		{
-			Vector3 point = camera.WorldToViewportPoint(target.position);
-			Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+		if (target) {
+			Vector3 point = c.WorldToViewportPoint (target.position);
+			Vector3 delta = target.position - c.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, point.z));
 			Vector3 destination = transform.position + delta;
-			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
-		}   
+			transform.position = Vector3.SmoothDamp (transform.position, destination, ref velocity, dampTime); 
+		}
 	}
 }
--- GamePlan/Assets/Scripts/PlayerMovement.cs
+++ GamePlan/Assets/Scripts/PlayerMovement.cs
@@ -12,20 +12,27 @@ public class PlayerMovement : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		//determine direction to move in
 		moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+
+		//if input isnt 0, we have a direction, so face that direction
 		if (Input.GetAxisRaw ("Horizontal") != 0 || Input.GetAxisRaw ("Vertical") != 0) {
 			transform.rotation = Quaternion.LookRotation (moveDirection);
 		}
 
+		//make it go faster
 		moveDirection *= speed * Time.deltaTime;
+
+		//do the move
 		transform.Translate (moveDirection, Space.World);
 
+		//coming soon (tm)
 		if(Input.GetButtonDown("Jump")) {
 			Jump();
 		}
 	}
 
-	private void Jump(){
+	protected void Jump(){
 
 	}
 }
