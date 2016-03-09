112a2f846a74adab95253722b11d6ba67d53efc4
TGoTaW <melovechicken@gmail.com>
2016-03-09 10:43:40 -0500

    added enemy and turning stuff

---
+++ GamePlan/Assets/Scripts/BasicEnemyScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicEnemyScript : MonoBehaviour {
+	private float health;
+	// Use this for initialization
+	void Start () {
+		
+		health = 120;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter (Collision col){
+		
+		if (col.gameObject.tag == "Weapon") {
+			Destroy (gameObject);
+		}
+
+	}
+
+}
