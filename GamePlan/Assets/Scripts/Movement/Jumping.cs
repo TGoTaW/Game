e8003a429acb4d4da416b6549da8814a3f719d4e
Michael T. Sheron <michael.sheron@yahoo.com>
2016-03-09 22:21:36 -0500

    doing stuff

---
+++ GamePlan/Assets/Scripts/Movement/Jumping.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jumping : MonoBehaviour {
+
+	private bool isGrounded;
+	public float jumpHeight = 10.0f;
+	float distanceToGround = 0;
+	RaycastHit hit;
+	private CharacterController cc;
+
+	// Use this for initialization
+	void Start () {
+		if (cc == null) {
+			cc = GetComponent<CharacterController> ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("Distance To Ground: " + distanceToGround);
+		Debug.Log ("Is Grounded: " +  isGrounded);
+
+		if (Physics.Raycast (transform.position, Vector3.down, out hit, 100.0F)) {
+			distanceToGround = hit.distance;
+		
+			if (distanceToGround <= 2) {
+				isGrounded = true;
+			}
+		}
+
+		//coming soon (tm)
+		if(Input.GetButtonDown("Jump") && isGrounded) {
+			Jump();
+		}
+	}
+
+	protected void Jump (){
+		
+		isGrounded = false;
+	}
+}
---
+++ GamePlan/Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private Vector3 moveDirection = Vector3.zero;
+	public float speed = 1.0f;
+
+	void Start(){
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//determine direction to move in
+		moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+		//face it
+		if (Input.GetAxisRaw ("Horizontal") != 0 || Input.GetAxisRaw ("Vertical") != 0) {
+			transform.rotation = Quaternion.LookRotation (moveDirection);
+		}
+		//move faster & evenly in all directions
+		moveDirection = moveDirection.normalized * speed * Time.deltaTime;
+		//do the move
+		transform.Translate (moveDirection, Space.World);
+	}
+}
