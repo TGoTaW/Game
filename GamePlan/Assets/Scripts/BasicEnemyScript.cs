4c26ff96d80b1c1d3ec42b3e0ba26be5ed3adf0e
TGoTaW <melovechicken@gmail.com>
2016-03-11 20:40:06 -0500

    Camerea fix, enemy update
    -camera looks better in perspective
    -added chase script to enemies
    -axe should damage when the model attacks with it
    -enemies should take 5 hits to kill (bugfix-hack)

--- GamePlan/Assets/Scripts/BasicEnemyScript.cs
+++ GamePlan/Assets/Scripts/BasicEnemyScript.cs
@@ -4,31 +4,54 @@ using System.Collections;
 public class BasicEnemyScript : MonoBehaviour {
 	private int health;
 	private Renderer rend;
+	private bool justCollided;
+	private Transform playerTransform;
+	private int speed = 5;
+	private float chaseRange = 15;
+	private float range;
+	private float stopRange = 2.5f;
+
 	// Use this for initialization
 	void Start () {
 		
 		health = 200;
 		rend = gameObject.GetComponent<Renderer> ();
-
+		justCollided = false;
+		playerTransform = GameObject.Find ("Player").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		range = Vector3.Distance (transform.position, playerTransform.position);
+		if (range <= stopRange) {
+			//TODO
+		}
+		else if (range <= chaseRange) {
+			transform.LookAt (playerTransform);
+			transform.Translate (speed * Vector3.forward * Time.deltaTime);
+		}
+
 	}
 
 	void OnCollisionEnter (Collision col){
 		
-		if (col.gameObject.tag == "Weapon") {
+		if (col.gameObject.tag == "Weapon" && justCollided == false) {
 			health -= 40;
-			StartCoroutine (BlinkAnimation());
+			justCollided = true;
+			StartCoroutine (BlinkAnimation ());
 			if (health <= 0) {
 
 				Destroy (gameObject);
 			}
+
+		} else {
+			justCollided = false;
 		}
 
+
 	}
+
 
 	IEnumerator BlinkAnimation(){
 		rend.material.color = Color.red;
